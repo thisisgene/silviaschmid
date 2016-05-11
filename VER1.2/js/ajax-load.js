a4b39dcb25cae01066b27e7db023e970693fcb58
friedrich15 <emdo2000@gmail.com>
2016-05-11 18:47:46 +0200

    Added animated gif to home screen.

--- VER1.2/js/ajax-load.js
+++ VER1.2/js/ajax-load.js
@@ -95,10 +95,13 @@ function finishedLoading(responseHtml, bonus) {
   $('#content-container').removeClass('loading');
   mainScript();
   if (!$('#content').hasClass('logo-intro')){
+    $('#content-container-home').fadeOut(300);
+    $('#content-container').fadeIn(300);  
     $('#logo').delay(300).fadeIn(300);
     $('.logo-intro').removeClass('logo-intro');
   }
   else {
+    $('#content-container-home').fadeIn(300);
     $('#logo').fadeOut(100);
     $('#content').addClass('logo-intro');
     $('#header-container').addClass('logo-intro');
